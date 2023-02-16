4fe0626eacee47f6f65f92da355973d4f5232a4c
udk307 <100525439+udk307@users.noreply.github.com>
2023-02-16 18:56:28 +0700

    Add files via upload

---
+++ Windows Auto update silent script.ps1
@@ -0,0 +1,4 @@
+﻿Set-ExecutionPolicy Bypass -Force
+Install-PackageProvider -Name "nuget" -RequiredVersion "2.8.5.201" -Force
+Install-Module PSWindowsUpdate -Force
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot | Out-File "C:\WindowsUpdates.log" -Force
