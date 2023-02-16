9fe56e0a26799c94222f090b6cfad021e8a619bc
udk307 <100525439+udk307@users.noreply.github.com>
2023-02-16 17:49:23 +0530

    Update Windows Auto update silent script.ps1

--- Windows Auto update silent script.ps1
+++ Windows Auto update silent script.ps1
@@ -1,4 +1,9 @@
-﻿Set-ExecutionPolicy Bypass -Force
+# Script to Automation windows update
+
+Set-ExecutionPolicy Bypass -Force
+
 Install-PackageProvider -Name "nuget" -RequiredVersion "2.8.5.201" -Force
+
 Install-Module PSWindowsUpdate -Force
+
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot | Out-File "C:\WindowsUpdates.log" -Force
