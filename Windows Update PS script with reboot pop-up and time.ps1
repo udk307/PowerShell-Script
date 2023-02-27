4624b770bb4421cb9267fd6b10b94c361c8706f8
udk307 <100525439+udk307@users.noreply.github.com>
2023-02-27 15:24:08 +0530

    Add files via upload

---
+++ Windows Update PS script with reboot pop-up and time.ps1
@@ -0,0 +1,20 @@
+﻿Set-ExecutionPolicy Bypass -Force
+
+Install-PackageProvider -Name "nuget" -Force
+
+if (Get-PackageProvider -ListAvailable -Name NuGet) {
+    Write-Host "NuGet Installed"
+}
+else {
+        Install-PackageProvider -Name "nuget" -Force  
+}
+
+if (Get-Module -ListAvailable -Name PSWindowsUpdate) {
+    Write-Host "PSWindowsUpdate Installed"
+}
+else {
+    
+        Install-Module PSWindowsUpdate -Force
+}
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll | Out-File "C:\WindowsUpdates.log" -Force |
+Shutdown.exe /F /R /T 120
